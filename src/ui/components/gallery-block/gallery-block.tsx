bafb1a62833122fdb9526a439a314d4a98cede57
Christiaan Bakker <christiaan.bakker@wearetriple.com>
2023-04-03 18:01:52 +0200

    feat: added initial gallery block

---
+++ src/ui/components/gallery-block/gallery-block.tsx
@@ -0,0 +1,8 @@
+import type * as React from "react";
+import $ from "./gallery-block.module.scss";
+
+const GalleryBlock: React.FC = () => {
+  return <div className={$.block}></div>;
+};
+
+export default GalleryBlock;
