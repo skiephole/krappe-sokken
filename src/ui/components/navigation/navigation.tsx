f4ab06554868362e6003364883dba03682eb2f11
Christiaan Bakker <christiaan.bakker@wearetriple.com>
2023-07-10 10:53:40 +0200

    Added TikTok link

--- src/ui/components/navigation/navigation.tsx
+++ src/ui/components/navigation/navigation.tsx
@@ -1,10 +1,17 @@
 import type * as React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faInstagram, faYoutube } from "@fortawesome/free-brands-svg-icons";
+import {
+  faInstagram,
+  faTiktok,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
 import $ from "./navigation.module.scss";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { useBreakpoints } from "app/hooks/use-window-width";
 
 const Navigation: React.FC = () => {
+  const { isSmall } = useBreakpoints();
+
   return (
     <nav className={$.navigation}>
       <div className={$.logoWrapper}>
@@ -14,19 +21,25 @@ const Navigation: React.FC = () => {
       </div>
       <div className={$.contactWrapper}>
         <a href="mailto: krappesokkenband@gmail.com" className={$.contactIcon}>
-          <FontAwesomeIcon icon={faEnvelope} size="2x" />
+          <FontAwesomeIcon icon={faEnvelope} size={isSmall ? "1x" : "2x"} />
         </a>
         <a
           href="https://www.youtube.com/@KrappeSokken/"
           className={$.contactIcon}
         >
-          <FontAwesomeIcon icon={faYoutube} size="2x" />
+          <FontAwesomeIcon icon={faYoutube} size={isSmall ? "1x" : "2x"} />
         </a>
         <a
           href="https://www.instagram.com/krappesokkenband/"
           className={$.contactIcon}
         >
-          <FontAwesomeIcon icon={faInstagram} size="2x" />
+          <FontAwesomeIcon icon={faInstagram} size={isSmall ? "1x" : "2x"} />
+        </a>
+        <a
+          href="https://www.tiktok.com/@krappe.sokken"
+          className={$.contactIcon}
+        >
+          <FontAwesomeIcon icon={faTiktok} size={isSmall ? "1x" : "2x"} />
         </a>
       </div>
     </nav>
