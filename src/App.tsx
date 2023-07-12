aeae843ac658d1105c460491c0dc56477e2f9df7
Christiaan Bakker <christiaan.bakker@wearetriple.com>
2023-07-12 14:49:43 +0200

    fix: make use of ga4

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import BasePageView from "./ui/views/base-page";
+import { Routes, Route } from "react-router-dom";
 import "../src/ui/styles/global.scss";
 
 function App() {
-  return <BasePageView />;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<BasePageView />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import TagManager from "react-gtm-module";
 import "./index.css";
 import App from "./App";
-
-const tagManagerArgs = {
-  gtmId: "G-7STC6XK71K",
-};
-
-TagManager.initialize(tagManagerArgs);
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import ReactGA from "react-ga4";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+//Initialize GA4
+ReactGA.initialize("G-7STC6XK71K");
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
+
+const SendAnalytics = () => {
+  ReactGA.send({
+    hitType: "pageview",
+    page: window.location.pathname,
+  });
+};
+
+reportWebVitals(SendAnalytics);
