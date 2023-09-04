f80d307daf7ef9ef9a21111524afeaf3382c063f
Christiaan Bakker <christiaan.bakker@wearetriple.com>
2023-09-04 07:00:43 +0200

    fix: add recestival and removed old data

--- src/ui/components/calendar-block/calendar-block.tsx
+++ src/ui/components/calendar-block/calendar-block.tsx
@@ -11,24 +11,6 @@ const CalendarBlock: React.FC = () => {
         </div>
         <h2 className={$.mobileHeader}>Opkomende gigs:</h2>
         <a
-          href="https://www.mixtream.nl/"
-          target="_blank"
-          rel="noreferrer"
-          className={$.show}
-        >
-          <b className={$.date}>27/08 Mixtream Festival</b>
-          <span className={$.title}>Rosarium Park, HHW</span>
-        </a>
-        <a
-          href="https://www.facebook.com/BergenLiveEvent/"
-          target="_blank"
-          rel="noreferrer"
-          className={$.show}
-        >
-          <b className={$.date}>02/09 Bergen Live</b>
-          <span className={$.title}>Centrum Bergen</span>
-        </a>
-        <a
           href="https://volta-amsterdam.nl/"
           target="_blank"
           rel="noreferrer"
@@ -36,6 +18,15 @@ const CalendarBlock: React.FC = () => {
         >
           <b className={$.date}>07/09 Volta</b>
           <span className={$.title}>Houtmankade 336, Amsterdam</span>
+        </a>
+        <a
+          href="https://hal25.nl/events/recestival/"
+          target="_blank"
+          rel="noreferrer"
+          className={$.show}
+        >
+          <b className={$.date}>16/09 Recestival</b>
+          <span className={$.title}>HAL25, Alkmaar</span>
         </a>
         <a
           href="https://kompleks.nl/event/popmania/"
