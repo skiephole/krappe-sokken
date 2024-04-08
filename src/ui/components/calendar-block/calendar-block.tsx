d492298f1a86e6a8ded3477d3c53ddf0f4fee350
Christiaan Bakker <christiaan.bakker@wearetriple.com>
2024-04-08 08:16:15 +0200

    chore: updated calender

--- src/ui/components/calendar-block/calendar-block.tsx
+++ src/ui/components/calendar-block/calendar-block.tsx
@@ -15,15 +15,6 @@ const CalendarBlock: React.FC = () => {
         </div>
         <h2 className={$.mobileHeader}>Opkomende gigs:</h2>
         <a
-          href="https://www.facebook.com/alkmaarsbierhuis/"
-          target="_blank"
-          rel="noreferrer"
-          className={$.show}
-        >
-          <b className={$.date}>05/04 Optreden Bierhuis</b>
-          <span className={$.title}>Alkmaar</span>
-        </a>
-        <a
           href="https://tenwesten.nl/agenda/gas-erop-ska-punk-rock-disco-house/"
           target="_blank"
           rel="noreferrer"
@@ -38,8 +29,44 @@ const CalendarBlock: React.FC = () => {
           rel="noreferrer"
           className={$.show}
         >
-          <b className={$.date}>26/04 Koningsnacht </b>
+          <b className={$.date}>26/04 21:00 Koningsnacht </b>
           <span className={$.title}>Café Taverne Bergen</span>
+        </a>
+        <a
+          href="https://hal25.nl/events/hofleverancier-25/"
+          target="_blank"
+          rel="noreferrer"
+          className={$.show}
+        >
+          <b className={$.date}>27/04 Koningsdag </b>
+          <span className={$.title}>HAL25 Alkmaar</span>
+        </a>
+        <a
+          href="https://www.facebook.com/cafedebuurenburgerbrug?locale=nl_NL"
+          target="_blank"
+          rel="noreferrer"
+          className={$.show}
+        >
+          <b className={$.date}>11/05 Optreden </b>
+          <span className={$.title}>Café de Buuren Burgerbrug</span>
+        </a>
+        <a
+          href="https://kunstcultuurweekend.wixsite.com/kcwoudorp/festivals"
+          target="_blank"
+          rel="noreferrer"
+          className={$.show}
+        >
+          <b className={$.date}>12/05 13:00 Kunst en Cultuurweekend </b>
+          <span className={$.title}>Koel 310 Alkmaar</span>
+        </a>
+        <a
+          href="https://huisweidfestival.nl/"
+          target="_blank"
+          rel="noreferrer"
+          className={$.show}
+        >
+          <b className={$.date}>19/05 20:45 Huisweid Festival</b>
+          <span className={$.title}>Tuitjenhorn</span>
         </a>
       </div>
     </div>
